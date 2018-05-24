89fe3a1ce89e8f15039d16f18420fbd43bf702dd
jobsjinu@gmail.com <jobsjinu@gmail.com>
2018-05-23 22:52:36 -0400

    parish dropdown and sponsor code

--- src/app/feature/sponsor/sponsor.component.ts
+++ src/app/feature/sponsor/sponsor.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Sponsor } from '../model/sponsor';
 import { SponsorService } from '../shared/service/sponsor.service';
 import { Router } from '@angular/router';
+import { Parish } from '../model';
+import { AdminService } from '../shared/service/admin.service';
+
 
 @Component({
   selector: 'app-sponsor',
@@ -11,15 +14,43 @@ import { Router } from '@angular/router';
 export class SponsorComponent implements OnInit {
 
   public sponsors: Array<Sponsor>;
+  parishes: Array<Parish>;
+  selectedParish: number;
+  displaySponsorList: boolean = false;
+  error: any;
+  message: any;
 
-  constructor(private router: Router, private sponsorService: SponsorService<Sponsor>) { }
+  constructor(private router: Router, 
+              private sponsorService: SponsorService<Sponsor>,
+              private adminService: AdminService<Parish>) { }
 
   ngOnInit() {
-    this.sponsorService.getSponsors().subscribe(
-      data =>
-        this.sponsors = data,
-      err => this.handleError
-    );
+    this.adminService.get('/api/admin/parishes')
+      .subscribe(
+        data => this.parishes = data,
+        err => console.log(err)
+      );
+    this.message = 'Please select a parish to see the sponsors.'
+  }
+
+  onParishSelect(parishId: number) {
+    if (parishId != 0) {
+      this.selectedParish = parishId;
+      this.message = null;
+      this.sponsorService.getSponsorsByParishId(parishId).subscribe(
+        data => {
+          this.sponsors = data
+          if(this.sponsors.length>1){
+            this.displaySponsorList = true;
+          }else{
+            this.displaySponsorList = false;
+          }
+        },
+        err => this.handleError
+       );
+    } else {
+      this.message = 'Please select a parish to see the sponsors.'
+    }
   }
 
   addSponser(): void {
